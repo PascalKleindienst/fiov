67a117a883f0f27191a390693a54d7f90cdd7ff9
Pascal Kleindienst <mail@pascalkleindienst.de>
2025-09-18 17:55:19 +0200

    docs: fix favicons

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,13 @@ export default defineConfig({
     // Site metadata
     title: 'Fiov',
     description: 'Financial Overview',
-    head: [['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }]],
+    head: [
+        ['link', { rel: 'icon', href: baseUrl + 'favicon-96x96.png', type: 'image/png', sizes: '96x96' }],
+        ['link', { rel: 'icon', href: baseUrl + 'logo.svg', type: 'image/svg+xml' }],
+        ['link', { rel: 'shortcut icon', href: baseUrl + 'favicon.ico' }],
+        ['link', { rel: 'apple-touch-icon', href: baseUrl + 'apple-touch-icon.png', sizes: '180x180' }],
+        ['link', { rel: 'manifest', href: baseUrl + 'site.webmanifest' }]
+    ],
     base: '/fiov/',
 
     // Theme configuration
